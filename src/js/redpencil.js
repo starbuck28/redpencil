ad7e59cbaaf962113a7609bb1d9e3a0ebe684a0d
Elaine Oglesbay <elaine.oglesbay@gmail.com>
2016-11-25 19:40:48 -0600

    Started overall plan for project in redpencil.js: Parameters for red pencil promotion, creation of item objects, ways to change item price, calculate price percentage reduction, track price stability, need to countdown days under promotion

--- src/js/redpencil.js
+++ src/js/redpencil.js
@@ -4,3 +4,32 @@ var RedPencil = (function() {
     pencil: pencil
   };
 })();
+
+//Old Price
+//New Price
+
+//Red Pencil Promotion:
+  //Triggered automatically when item price is reduced by 5-30%, price has been stable for 30 days, has not had RPP for those 30 days
+  //Price is set to be reduced for 30 days
+  //Can continue if price is further reduced to max of 30%, but time is not prolonged
+  //Ends if
+    //Price increased
+    //Price reduced more than 30%
+    //RPP has lasted > 30 days
+
+//Need way to keep track of items
+  //Item object with
+    //Item name
+    //Item original price
+    //Item new price
+    //Percent off the price
+    //RPP or no
+    //RPP start date (can be N/A)
+    //RPP stop date (can be N/A)
+    //Days price stable
+
+//Need way to reduce item price
+//Need way to increase item price
+//Need way to calculate price reduction percentage
+//Need way to track price stability
+//Need way to countdown RPP
